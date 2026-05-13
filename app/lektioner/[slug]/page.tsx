41820584c845b1817f30e29960e1624d5b41db42
pederhofmanbang <peder.hofman-bang@intellectualcapital.se>
2026-05-13 12:33:39 +0200

    feat(lessons): interactive "Pizza-Ägaren" game for lesson 01 (#9)

--- app/lektioner/[slug]/page.tsx
+++ app/lektioner/[slug]/page.tsx
@@ -69,6 +69,7 @@ export default function LessonPage({ params }: { params: Params }) {
       </aside>
 
       <LessonTabs
+        slug={lesson.slug}
         media={lessonMedia[lesson.slug] ?? {}}
         flashcards={lessonFlashcards[lesson.slug]}
         code={lesson.body.code}
